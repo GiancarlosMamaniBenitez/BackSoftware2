b0f2b2cd468759b1941a2e1edcadeb8dc8878512
veilhdmi <juniorhuamanro@gmail.com>
2023-10-20 09:11:35 -0500

    version-final

--- functions/index.js
+++ functions/index.js
@@ -37,6 +37,53 @@ app.post('/api/create', async (req,res) => {
     return res.status(200).send('new usuario creado')
 });
 
+
+app.post('/usuarios/:id/crear-tarjeta', async (req, res) => {
+    const {
+        number,
+            mm,
+            yyyy,
+            cvv,
+            cardsname,
+            selectedCard,
+            incomes,
+            expenses,
+            categories,
+            spendingLimit,
+            savingsGoal
+    } = req.body
+    const dataSend = {
+        number: number,
+            mm: mm,
+            yyyy: yyyy,
+            cvv: cvv,
+            cardsname: cardsname,
+            selectedCard: selectedCard,
+            incomes: incomes,
+            expenses: expenses,
+            categories: categories,
+            spendingLimit: spendingLimit, // Nuevo atributo
+            savingsGoal: savingsGoal,
+    }
+    const {id} = req.params
+    const doc = db.collection("Usuario")
+    await doc.doc(id).collection("Tarjeta").add(dataSend)
+
+    return res.status(200).send('new usuario creado')
+})
+
+app.get('/usuarios', async (req, res) => {
+    
+    const doc = await db.collection("Usuario").get()
+    const Usuarios = doc.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+
+    res.send(Usuarios)
+
+})
+
 //get usuario
 app.get("/api/get/:id", async (req,res)=>{
     try {
@@ -55,6 +102,36 @@ app.get("/api/get/:id", async (req,res)=>{
         return res.status(500).send("Internal Server Error");
     }
 });
+
+app.get('/usuarios/:id/tarjetas', async (req, res) => {
+    const {id} = req.params
+    const doc = await db.collection("Usuario")
+    const docRef = await doc.doc(id)
+    const subCol = await docRef.collection("Tarjeta").get()
+    
+
+    const Tarjetas = subCol.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+
+    res.send(Tarjetas)
+
+})
+
+app.get('/usuarios/:id/tarjetas/:tarjetaId', async (req, res) => {
+    const {id} = req.params
+    const doc = await db.collection("Usuario")
+    const docRef = await doc.doc(id)
+    const subCol = await docRef.collection("Tarjeta")
+    const docSubCol = await subCol.doc(req.params.tarjetaId).get()
+    
+    res.send({
+        id: docSubCol.id,
+        ...docSubCol.data(),
+    })
+
+})
 
 app.get('/delete-usuario/:id', async (req,res)=>{
     await db.collection('Usuario').doc(req.params.id).delete()
